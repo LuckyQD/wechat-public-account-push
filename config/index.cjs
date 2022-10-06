dd0d64337b0ffc9d61c7a725528c59ee18796aa8
LuckyQD <115124833+LuckyQD@users.noreply.github.com>
2022-10-06 10:43:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKYHa6WCv-ewJdeorKXcohFitqAE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'WCWsRL7VbOiBAjgnZODdqkOz1560Koe67yv0039Dtso',
+      useTemplateId: 'acz9D3ZvCvceUuxzUP7j97xVf8xeu3wieMCmz_EL9gY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
