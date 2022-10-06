790f8cab1421a0a8dd8ea788adae157559963fc0
LuckyQD <115124833+LuckyQD@users.noreply.github.com>
2022-10-06 09:51:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx6cbf8a4b1411441e',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '674b44cedf6f4703042f222bec69efca',
 
   PROVINCE: '广东',
   CITY: '惠州',
